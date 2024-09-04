e9768244cafaceae6c2a87509733c969bc257ed6
jmcdonough1 <ufjohn2003@gmail.com>
2024-09-03 20:02:15 -0400

    Add files via upload

---
+++ pythons interactive shell.py
@@ -0,0 +1,10 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("John McDonough")
+John McDonough
+>>> print ("COP2002-0M1")
+COP2002-0M1
+>>> print ("Fall 2024")
+Fall 2024
+>>> print ("This is an interactive session")
+This is an interactive session
