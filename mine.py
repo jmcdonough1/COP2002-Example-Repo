9cff895c7274ff4ee1f2d23989a4ef528cafd01b
jmcdonough1 <ufjohn2003@gmail.com>
2024-09-04 14:23:47 -0400

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,9 +1,9 @@
 # Write a line of code that prints "Hello world!".
-
+print("Hello world!")
 
 def hello_world():
   # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
-  
+  varible_name="Hello world"
   return variable_name
 
 
